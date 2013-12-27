bfddfbab25b5fbf95da02721680fde327e1ce986
xifle <github@makeroid.eu>
2013-12-27 16:18:25 +0100

    Configuration change: filamentexchange disabled

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -398,7 +398,7 @@ const unsigned int dropsegments=5; //everything with less than this number of st
 
 //adds support for experimental filament exchange support M600; requires display
 #ifdef ULTIPANEL
-  #define FILAMENTCHANGEENABLE
+  //#define FILAMENTCHANGEENABLE
   #ifdef FILAMENTCHANGEENABLE
     #define FILAMENTCHANGE_XPOS 3
     #define FILAMENTCHANGE_YPOS 3
